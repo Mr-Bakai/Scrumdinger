ac6f48f28113eb3f427a74d9cf9d17389a15c004
Mr-Bakai <ismailov.time@gmail.com>
2023-01-29 17:09:14 +0600

    Added: speechRecognizer

--- Scrumdinger/Helpers/ScrumProgressViewStyle.swift
+++ Scrumdinger/Helpers/ScrumProgressViewStyle.swift
@@ -1,6 +1,9 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-*/
+//
+//  ScrumProgressViewStyle.swift
+//  Scrumdinger
+//
+//  Created by Bakai Ismailov on 27/1/23.
+//
 
 import SwiftUI
 
